13ddbe66c773ee357e56a42e24c5ab34caf3f2ae
randilfernando <randil.fernando@outlook.com>
2018-04-08 15:27:17 +0530

    use github as a maven repository

---
+++ copy-jar-files.sh
@@ -0,0 +1,5 @@
+rm -rf jars
+mkdir jars
+cp ./ibuy-msf4j/order-service-msf4j/target/order-service-msf4j-0.1.jar ./jars/
+cp ./ibuy-msstack/order-service-msstack/target/order-service-msstack-1.0.jar ./jars/
+cp ./ibuy-spring-boot/order-service-spring-boot/target/order-service-spring-boot-1.0.jar ./jars/
